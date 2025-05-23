0f8b80ed8c0bbcbf397be2ff326dd835ad3b6d8a
Naveenkumar <naveennk045@gmail.com>
2025-05-23 17:32:19 +0530

    Code refactoring

---
+++ src/problems/companywise/microsoft/MenuCard/MenuCard.java
@@ -0,0 +1,52 @@
+package problems.companywise.microsoft.MenuCard;
+
+import java.util.Scanner;
+
+public class MenuCard {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("""
+                pizza=200
+                burger =150
+                biriyani = 250
+                dosa =80
+                idly = 50
+                """);
+
+        System.out.print("Enter Number of items : ");
+        int n= scan.nextInt();
+
+        int total=0;
+        for (int i = 0; i < n; i++) {
+
+            System.out.print("Enter Item "+(i+1)+" : ");
+            String item= scan.next().toLowerCase();
+            switch(item){
+                case "pizza" :
+                    total+=200;
+                    break;
+                case "burger" :
+                    total+=150;
+                    break;
+                case "biriyani" :
+                    total+=250;
+                    break;
+                case "dosa":
+                    total+=80;
+                    break;
+                case "idly":
+                    total+=50;
+                    break;
+                default:
+                    System.out.println("Choose your Item");
+                    break;
+             }
+        }
+        System.out.println("Total Amount ₹"+total);
+    if(total>500){
+        total=(int)(total*0.9);
+        System.out.println("Total Amount after discount is ₹"+total);
+    }
+    }
+}
---
+++ src/problems/companywise/microsoft/ticketbooking/TicketBooking.java
@@ -0,0 +1,38 @@
+package problems.companywise.microsoft.ticketbooking;
+
+import java.util.Scanner;
+
+public class TicketBooking {
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print("Enter no. of Tickets you want book : ");
+        int quantity= scan.nextInt();
+
+        int age;
+        int sum=0;
+        for (int i = 0; i <quantity ; i++) {
+            System.out.print("Enter Age: ");
+            age= scan.nextInt();
+
+            if(age<=12){
+                sum+=20;
+            }
+            else if(age>12 && age<60){
+                sum+=50;
+            }
+            else {
+                sum+=30;
+            }
+
+        }
+
+        System.out.println("Total Quantity : "+quantity);
+        System.out.println("Total Amount : "+sum);
+
+    }
+
+
+
+}
