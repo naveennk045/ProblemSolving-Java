22bb9729be4ccaeadf4af6dffb3e991620c1450a
naveennk045 <naveennk045@gmail.com>
2025-10-04 22:36:45 +0530

    problem(he): working comparetriplets in hackerrank

---
+++ src/hackerrank/CompareTriplets.java
@@ -0,0 +1,9 @@
+package hackerrank;
+
+// https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
+public class CompareTriplets {
+
+    public static void main(String[] args) {
+
+    }
+}
