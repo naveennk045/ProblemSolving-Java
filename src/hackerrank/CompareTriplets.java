3c26f1668b08b960e406da8772844aa1db1873c8
naveennk045 <naveennk045@gmail.com>
2025-10-04 22:44:23 +0530

    problem(he): working comparetriplets in hackerrank

--- src/hackerrank/CompareTriplets.java
+++ src/hackerrank/CompareTriplets.java
@@ -1,7 +1,22 @@
 package hackerrank;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
 public class CompareTriplets {
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int aliceScore = 0;
+        int bobScore = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) > b.get(i)) aliceScore += 1;
+            else if (a.get(i) < b.get(i)) bobScore += 1;
+        }
+
+        return new ArrayList<>(Arrays.asList(aliceScore, bobScore));
+    }
+
 
     public static void main(String[] args) {
 
